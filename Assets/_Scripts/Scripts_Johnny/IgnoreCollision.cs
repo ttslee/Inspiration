097a00968b487106f1e2bdb56d5b48000ab1c21c
Johnny Ngo <jdngo2@uci.edu>
2020-03-27 17:59:49 -0700

    Limbs ignore each other

---
+++ Assets/_Scripts/Scripts_Johnny/IgnoreCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    //This script is mainly used to prevent Player limbs from touching eachother
+    void Start()
+    {
+        var colliders = GetComponentsInChildren<Collider2D>(); //List of all Colliders under Parent
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            for(int j = i + 1; j < colliders.Length; j++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[j % colliders.Length]);
+            }
+        }
+    }
+}
