a2ab7846f01a7296ae1ae1ae8eedb4a950500fde
ttslee <theodol2@uci.edu>
2020-03-28 03:17:51 -0700

    merge

---
+++ Assets/_Scripts/Teo/CursorRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorRotation : MonoBehaviour
+{
+    public float speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/_Scripts/Teo/NetworkManagerEngarde.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+namespace Teo
+{
+    
+    [AddComponentMenu("")]
+    public class NetworkManagerEngarde : NetworkManager
+    {
+        public Transform player1Spawn;
+        public Transform player2Spawn;
+        // Start is called before the first frame update
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            // add player at correct spawn position
+            Transform start = numPlayers == 0 ? player1Spawn : player2Spawn;
+            GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+            NetworkServer.AddPlayerForConnection(conn, player);
+
+            // spawn ball if two players
+            if (numPlayers == 2)
+            {
+                //ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+                //Mirror.NetworkServer.Spawn(ball);
+            }
+        }
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            // destroy ball
+            //if (ball != null)
+            //    NetworkServer.Destroy(ball);
+
+            // call base functionality (actually destroys the player)
+            base.OnServerDisconnect(conn);
+        }
+    }
+}
