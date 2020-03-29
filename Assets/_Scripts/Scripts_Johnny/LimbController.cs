bd290d4dfe18cfa0d7ada57beeae93ea1cb517be
Johnny Ngo <jdngo2@uci.edu>
2020-03-29 09:05:58 -0700

    trying to redo my things again

--- Assets/_Scripts/Scripts_Johnny/LimbController.cs
+++ Assets/_Scripts/Scripts_Johnny/LimbController.cs
@@ -11,13 +11,12 @@ namespace Engarde_Johnny.Player
 
         //Arm functions
         public bool useMousePos = false;
+        public bool dirFlip = false;
         public bool getRoots = false;
 
-
         public Vector2 targetPos; //pos we want to rotate to
-        private bool dirFlip = false;
         private bool sin = false;
-        private int force = 10; //controls how strong/fast a limb moves
+        public int force = 10; //controls how strong/fast a limb moves
 
         void Start()
         {
@@ -26,11 +25,6 @@ namespace Engarde_Johnny.Player
             HingeJoint2D hinge = GetComponent<HingeJoint2D>();
             while (hinge != null && hinge.transform != root)
             {
-                float dist = transform.position.x - root.position.x;
-                if (dist >= 0f)
-                    dirFlip = false;
-                else
-                    dirFlip = true;
                 limbs.Add(new Limb(hinge.attachedRigidbody, dirFlip, sin));
                 if (getRoots)
                     hinge = hinge.connectedBody.GetComponent<HingeJoint2D>();
